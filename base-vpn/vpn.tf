a178c1c13c05fb80b1d3af495799088d773a07c9
SecOpsCrooner <4hathacker@example.com>
2020-08-11 10:16:50 +0530

    Avoiding keys while tf template...

---
+++ base-vpn/vpn.tf
@@ -0,0 +1,81 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = "true"
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+# Put your IP here to whitelist it for ssh
+
+variable "access_addr" {
+  type    = string
+  default = "62.17.146.153/32"
+
+}
+
+resource "aws_security_group" "vpn_group" {
+  name        = "vpn_group"
+  description = "Allow Ports for VPN and SSH access"
+
+  # Open the default OpenVPN Port
+  ingress {
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["62.17.146.153/32"]
+  }
+
+
+
+  # ssh for remote access, might want to lock down to your IP prior to rolling out
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.access_addr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "primary_vpn" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.vpn_group.name]
+  key_name        = "primary-c2-key"
+
+
+  tags = {
+    Name = "Primary vpn"
+  }
+}
+
+output "IP" {
+  value = aws_instance.primary_vpn.public_ip
+}
+
+# Apply complete! Resources: 1 added, 0 changed, 0 destroyed.
+
+# Outputs:
+
+# IP = 3.128.24.63
